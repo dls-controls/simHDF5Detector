039eaea648c119865b2a6f6e14b00cc592d4bce5
Alan Greer <alan.greer@diamond.ac.uk>
2015-07-20 09:43:21 +0100

    Updated builder script to work with new ADCore version.

--- etc/builder.py
+++ etc/builder.py
@@ -2,34 +2,38 @@ from iocbuilder import Device, AutoSubstitution, Architecture, SetSimulation
 from iocbuilder.arginfo import *
 from iocbuilder.modules.asyn import Asyn, AsynPort, AsynIP
 
-from iocbuilder.modules.areaDetector import AreaDetector, _NDPluginBase, _ADBase, _ADBaseTemplate, simDetector
+from iocbuilder.modules.ADCore import ADCore, ADBaseTemplate, makeTemplateInstance, includesTemplates
 
-class _SimHDF5Detector(AutoSubstitution):
+@includesTemplates(ADBaseTemplate)
+class SimHDF5DetectorTemplate(AutoSubstitution):
     TemplateFile= "simHDF5Detector.template"
-    SubstitutionOverwrites = [_ADBaseTemplate]
 
-class SimHDF5Detector(_ADBase):
+class SimHDF5Detector(AsynPort):
     """Create an SimHDF5Detector detector"""
-    _BaseTemplate = _ADBaseTemplate
-    _SpecificTemplate = _SimHDF5Detector
-    AutoInstantiate = True
-    def __init__(self, DRIVER="CAM.SIM", BUFFERS=50, MEMORY=-1, **args):
-        self.__super.__init__(**args)
+    Dependencies = (ADCore,)
+    # This tells xmlbuilder to use PORT instead of name as the row ID
+    UniqueName = "PORT"
+    _SpecificTemplate = SimHDF5DetectorTemplate
+    def __init__(self, PORT, BUFFERS = 50, MEMORY = 0, **args):
+        # Init the superclass (AsynPort)
+        self.__super.__init__(PORT)
+        # Update the attributes of self from the commandline args
         self.__dict__.update(locals())
-        
+        # Make an instance of our template
+        makeTemplateInstance(self._SpecificTemplate, locals(), args)
+
     # __init__ arguments
-    ArgInfo = _ADBase.ArgInfo + _BaseTemplate.ArgInfo + makeArgInfo(__init__,
-        DRIVER = Simple('Name of the driver port', str),
+    ArgInfo = ADBaseTemplate.ArgInfo + _SpecificTemplate.ArgInfo + makeArgInfo(__init__,
+        PORT = Simple('Port name for the detector', str),
         BUFFERS = Simple('Maximum number of NDArray buffers to be created for plugin callbacks', int),
-        MEMORY  = Simple('Max memory to allocate, should be maxw*maxh*nbuffer for driver and all attached plugins', int))
+        MEMORY = Simple('Max memory to allocate, should be maxw*maxh*nbuffer for driver and all attached plugins', int))
+
+    # Device attributes
     LibFileList = ['simHDF5Detector']
     DbdFileList = ['simHDF5Support']
-    SysLibFileList = []
-    MakefileStringList = []
 
     def Initialise(self):
         print '# SimHDF5DetectorConfig(portName, maxBuffers, maxMemory )'
         print 'SimHDF5DetectorConfig( %(PORT)10s, %(BUFFERS)10d, %(MEMORY)9d )' % self.__dict__
-
 
 
